a9423fb2ac8b28f60d25a9a948104f18f7ff3f54
Dhel <nikolai@swrtech.io>
2023-01-31 17:32:43 +0800

    fix: reloading page doesn't render component

--- components/HomePlaylist.tsx
+++ components/HomePlaylist.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import { useRecoilValue, useRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import { ChevronDownIcon } from "@heroicons/react/outline";
 import { useRouter } from "next/router";
 
@@ -21,12 +21,11 @@ function HomePlaylist() {
     if(!session?.user && status !== 'loading' && status !== 'authenticated') {
       router.push('/login')
     }
-
   }, [session])
 
   useEffect(() => {
     if (spotifyApi.getAccessToken()) {
-      spotifyApi.getUserPlaylists().then((data) => {
+      spotifyApi.getUserPlaylists().then((data: any) => {
         setPlaylists(data.body.items);
       });
     }
@@ -34,7 +33,7 @@ function HomePlaylist() {
   }, [spotifyApi]);
 
   return (
-    <div className="flex-grow h-screen p-2 px-4 overflow-y-scroll scrollbar-hide">
+    <div className="flex-grow h-screen p-2 px-4 ">
       <header className="absolute top-5 right-8">
         <div
           className="flex items-center p-1 pr-2 space-x-3 text-white bg-black rounded-full cursor-pointer opacity-90 hover:opacity-80"
@@ -45,7 +44,7 @@ function HomePlaylist() {
             src={session?.user?.image}
             alt=""
           />
-          <h2>{session?.user.name}</h2>
+          <h2>{session?.user?.name}</h2>
           <ChevronDownIcon className="w-5 h-5" />
         </div>
       </header>
@@ -57,8 +56,7 @@ function HomePlaylist() {
           {playlists?.slice(0, 6).map((playlist) => (
             <div>
               <Link
-                href="/tracks/trackPlaylist"
-                as={`/tracks/${playlist.id}`}
+                href={`/tracks/${playlist.id}`}
                 onClick={() => setPlaylistId(playlist.id)}
               >
                 <CategoryCards
@@ -73,15 +71,14 @@ function HomePlaylist() {
       </div>
       <hr className="border-t-[0.1px] border-gray-900 mt-1" />
 
-      <div className="flex flex-col space-x-1 ">
+      <div className="flex flex-col space-x-1">
         <h2 className="pt-8 pl-4 mb-4 text-3xl text-white text-bold">
           Made For Z
         </h2>
-        <ul className="grid grid-flow-col gap-6">
+        <ul className="grid gap-4 md:grid-cols-4 sm:grid-cols-2 xl:grid-cols-8">
           {playlists?.map((item) => (
             <Link
-              href="/tracks/trackPlaylist"
-              as={`/tracks/${item.id}`}
+              href={`/tracks/${item.id}`}
               onClick={() => setPlaylistId(item.id)}
             >
               <Category
